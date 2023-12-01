7e6a02c4135d92488f7dad6a960ac403825de12c
mryan-3 <malawaryan@gmail.com>
2023-12-01 22:09:57 +0300

    A simple swapping program to practice on Pointers

--- pointer/main.go
+++ pointer/main.go
@@ -4,23 +4,30 @@ import (
 	"fmt"
 )
 
-type Player struct{
-    health int
+func swap(x, y *int){
+    temp := *x
+    //Initialize a temporary variable and assign it the value pointed by *x
+    *x = *y
+    *y = temp
+    //The value pointed to by x is set to the value pointed to by y.
+    //The value pointed to by y is set to the temporary value (temp), which o    riginally held the value pointed to by x.
 }
-
-func takeDamageFromExplosion(player *Player){
-    fmt.Println("Player is taking damage from explosion\n")
-    explosionDmg := 10
-    player.health -= explosionDmg
-}
-
 
 func main(){
-    player := &Player{
-        health: 100,
-    }
-    //it references to the pointer
-    fmt.Printf("Before explosion %+v\n", player)
-    takeDamageFromExplosion(player)
-    fmt.Printf("After explosion %+v", player)
+    fmt.Println("Simple Swappning Program to pracice on pointers")
+    //Initialize two variables
+    a := 5
+    b := 10
+
+    fmt.Printf("Before Swap a: %d, b: %d\n", a, b)
+
+    //Create two pointers to the variables
+    ptrA := &a
+    ptrB := &b
+
+    //Call the function with pointers
+    swap(ptrA, ptrB)
+
+    fmt.Printf("After swap - a: %d, b: %d\n", a, b)
+
 }
