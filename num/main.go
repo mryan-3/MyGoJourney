9947da78a4e8b93dfb51ba05c9f4b218e0a8fdd7
mryan-3 <malawaryan@gmail.com>
2023-12-15 05:18:52 +0300

    Numbers and simple operations

---
+++ num/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main (){
+    fmt.Println("Numbers")
+    lgNumber := 1000
+    lgNumber ++
+    lgNumber --
+    lgNumber *= lgNumber
+    lgNumber /= lgNumber
+    fmt.Println(lgNumber)
+}
