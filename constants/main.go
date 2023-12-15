c32e522b0bfc2c4b52f580add73cafaf73e127c0
mryan-3 <malawaryan@gmail.com>
2023-12-15 04:58:49 +0300

    Learned about constants and did a simple challenge

---
+++ constants/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main(){
+    fmt.Println("Constants")
+    const myConst = 20
+    const(
+        x = 23
+        y = 99
+        z = 57
+    )
+    fmt.Println(myConst)
+   fmt.Println(x)
+      fmt.Println(y)
+      fmt.Println(z)
+
+
+
+}
