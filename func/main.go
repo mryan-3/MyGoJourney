8109dfe9fa4069e1498cdb12ad80d3eb74f44b13
mryan-3 <malawaryan@gmail.com>
2023-12-16 13:23:33 +0300

    Slices as a parameter

--- func/main.go
+++ func/main.go
@@ -11,9 +11,17 @@ func double(nums ...int){
         fmt.Println(v * 2)
     }
 }
+func itter(x []int){
+    for i, v := range x {
+        fmt.Println(i ,v)
+    }
+}
 
 func main(){
     double(1, 2, 3, 45,4 , 56)
     greet("Reya", "Jones")
-
+    sliced := []int{
+        1, 2, 3 , 4,
+    }
+    itter(sliced)
 }
