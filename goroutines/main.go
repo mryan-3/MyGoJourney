d4388fae14440d7497c048cbc92bf9353fb30e7b
mryan-3 <malawaryan@gmail.com>
2024-01-19 17:19:54 +0300

    Learnt about adding goroutines to functions to speed up the process

--- goroutines/main.go
+++ goroutines/main.go
@@ -5,19 +5,49 @@ import (
 	"time"
 )
 
-func printNumbers(){
-    for i := 1; i <= 10; i++ {
-        time.Sleep(1 * time.Second)
-        fmt.Println(i)
-    }
-}
-func main(){
 
-    go printNumbers()
-    fmt.Println("Hello")
-    time.Sleep(6 * time.Second)
-    fmt.Println("Done")
+func main(){
+    start := time.Now()
+    defer func (){
+        fmt.Println(time.Since(start))
+    }()
+
+
+    evilNinjas := []string {"Tom", "John", "Bob", "Andy"}
+
+
+    for _, evilNinja := range evilNinjas{
+      go attack(evilNinja)
+    }
+
+    time.Sleep(2 * time.Second)
 }
+
+func attack(target string){
+    fmt.Println("Throwing stars at ", target)
+    time.Sleep(1 * time.Second)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //time.sleep serves two purposes:
 /*To simulate work and execution time in the goroutines. In real code, the goroutines would be doing actual work that takes time. But in these simple examples, time.Sleep gives an artificial delay to simulate the passage of time and work being done in the goroutine.
